8aa533657c2414750a9072e6c9831678847f72aa
Bilel <Bilel@MacBook-Pro-de-Snake.local>
2020-02-29 19:42:16 +0100

    first commit

---
+++ server/loaders/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+
+//tranking request optional
+// we create file and save all request history into access.log
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+module.exports = async app => {
+  app.use(cors("*"));
+  app.use(morgan("dev"));
+  app.use(express.json());
+};
