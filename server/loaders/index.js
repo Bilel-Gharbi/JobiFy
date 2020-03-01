87bdbc0a08df49aed9220a15b5e8437f295fe0eb
Bilel <Bilel@MacBook-Pro-de-Snake.local>
2020-03-01 16:10:05 +0100

    Mysql database online connection  setup done , env variable done

--- server/loaders/index.js
+++ server/loaders/index.js
@@ -1,4 +1,5 @@
 const expressLoader = require("./express");
+const dataBaseLoader = require("./dataBase");
 
 module.exports = async app => {
   try {
@@ -6,5 +7,11 @@ module.exports = async app => {
     console.log("Experss App Intialized");
   } catch (err) {
     console.log(err);
+  }
+  try {
+    await dataBaseLoader.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
   }
 };
